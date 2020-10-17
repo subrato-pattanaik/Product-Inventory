8cc35585203d1c9378b3cd2b7e47c59a925dfc55
subrato31 <subrato.pattanaik@gmail.com>
2020-10-17 14:01:43 +0530

    Add product list component

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,13 +1,56 @@
-import React from "react";
-import { withRouter } from "react-router-dom";
+import React, { useState } from "react";
+import { withRouter, useHistory } from "react-router-dom";
+import { Container, Row, Col, Button, Alert } from "react-bootstrap";
+import AboutProductList from "./AboutProductList";
+import ProductListTable from "./ProductListTable";
+
 function ProductList() {
+  let navigation = useHistory();
+  const [allSelected, setAllSelected] = useState(false);
+
   return (
-    <div>
-      <p>this is about section</p>
-      <p>this is aboasdut section</p>
-      <p>this is about section</p>
-      <p>this is about section</p>
-    </div>
+    <Container fluid className="mt-4 mb-5">
+      <Row>
+        <Col sm={2} className="border-right bg-light">
+          <AboutProductList />
+        </Col>
+        <Col sm={10}>
+          <Container>
+            <Row className="mt-2">
+              <Col sm={4}>
+                <h3 className="display-5 ml-4">Product List</h3>
+              </Col>
+
+              <Col sm={8} className="d-flex justify-content-end">
+                <Button
+                  variant="warning"
+                  className=" "
+                  onClick={() => navigation.push("./addProduct")}
+                >
+                  Add Product
+                </Button>
+              </Col>
+            </Row>
+            <hr />
+            {allSelected && (
+              <Alert variant="danger">
+                <Alert.Heading className="text-center small">
+                  All conversations on this page are selected.
+                </Alert.Heading>
+              </Alert>
+            )}
+            <Row>
+              <Col>
+                <ProductListTable
+                  allSelected={allSelected}
+                  setAllSelected={setAllSelected}
+                />
+              </Col>
+            </Row>
+          </Container>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
